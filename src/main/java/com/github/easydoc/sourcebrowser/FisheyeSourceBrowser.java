68dedf11dee98d78e048bcccd7c39fcaf2a23e9f
weekens <kvs16@yandex.ru>
2012-05-04 09:22:56 +0400

    + documentation for Fisheye source browser

--- src/main/java/com/github/easydoc/sourcebrowser/FisheyeSourceBrowser.java
+++ src/main/java/com/github/easydoc/sourcebrowser/FisheyeSourceBrowser.java
@@ -6,6 +6,19 @@ import java.net.URL;
 import com.github.easydoc.model.Doc;
 import com.github.easydoc.param.SourceBrowserParam;
 
+/*@@easydoc-start, belongs=easydoc-maven-source-browser, weight=50@@
+<p><b>fisheye</b></p>
+
+Fisheye source browser. To specify it, use the following snippet:<br>
+<pre>
+&lt;sourceBrowser&gt;
+	&lt;type&gt;fisheye&lt;/type&gt;
+	&lt;baseUrl&gt;https://mycompany.com/browse/myrepo/mysourcepath/&lt;/baseUrl&gt;
+&lt;/sourceBrowser&gt;
+</pre>
+<br>
+You need to check your Fisheye repository path by just going to the 'Source' section.
+@@easydoc-end@@*/
 public class FisheyeSourceBrowser implements SourceBrowser {
 	
 	private final SourceBrowserParam sbParam;
